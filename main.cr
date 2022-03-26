c9672efb45ffccc77b6b1244e320053dced2c0d0
Michael Tillman <mnamllit@gmail.com>
2022-03-26 18:58:57 -0500

    Added a player class and updated the display function to represent the board array.

--- main.cr
+++ main.cr
@@ -1,12 +1,26 @@
-board = ["", "", "", "", "", "", "", "", "",]
+board = ["", "", "", "", "", "", "", "", ""]
 
-def display()
-    puts "   |   |   |"
-    puts "--------------"
-    puts "   |   |   |"
-    puts "--------------"
-    puts "   |   |   |"
+def display(board)
+    puts "  #{board[0]} | #{board[1]} | #{board[2]}"
+    puts "----------"
+    puts "  #{board[3]} | #{board[4]} | #{board[5]}"
+    puts "----------"
+    puts "  #{board[6]} | #{board[7]} | #{board[8]}"
 end
 
 
-display()
+class Player
+    def initialize(name)
+        @name = name
+    end
+
+    def name
+        @name
+    end
+end
+
+x = Player.new "X"
+o = Player.new "O"
+
+
+display(board)
