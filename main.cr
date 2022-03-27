9b1713ba03098e8950e59deb1fca1c331f80ef66
Michael Tillman <mnamllit@gmail.com>
2022-03-26 21:27:12 -0500

    Added win conditions and a function to display the winner.

--- main.cr
+++ main.cr
@@ -46,6 +46,29 @@ def board_input(player, position, board)
     end
 end
 
+def win_conditions(board)
+    # Checking horizontally for win
+    if board[0] == board[1] && board[0] == board[2] && board[0] != " "
+        return true
+    elsif board[3] == board[4] && board[3] == board[5] && board[3] != " "
+        return true
+    elsif board[6] == board[7] && board[6] == board[8] && board[6] != " "
+        return true
+    # Checking vertically for win
+    elsif board[0] == board[3] && board[0] == board[6] && board[0] != " "
+        return true
+    elsif board[1] == board[4] && board[1] == board[7] && board[1] != " "
+        return true
+    elsif board[2] == board[5] && board[2] == board[8] && board[2] != " "
+        return true
+    # Checking diagonally for win
+    elsif board[0] == board[4] && board[0] == board[8] && board[0] != " "
+        return true
+    elsif board[2] == board[4] && board[2] == board[6] && board[2] != " "
+        return true
+    end
+end
+
 
 player_state = 0
 
@@ -68,10 +91,27 @@ while true
     else
         board_input(o, u_input, board)
     end
-    
+
+    if (win_conditions(board) == true)
+        break
+    end
+
     if player_state == 0
         player_state = 1
     else
         player_state = 0
     end
 end
+
+def game_winner(board, player_state)
+    display(board)
+
+    if player_state == 0
+        puts "Player 1 Wins!"
+    else
+        puts "Player 2 Wins!"
+    end
+end
+
+
+game_winner(board, player_state)
