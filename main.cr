fc40935fcfdfea120bfbc014dead19d6ce9c237c
Michael Tillman <mnamllit@gmail.com>
2022-03-26 19:10:18 -0500

    Added the basic loop to cycle through the array and display.

--- main.cr
+++ main.cr
@@ -1,21 +1,23 @@
 board = ["", "", "", "", "", "", "", "", ""]
 
 def display(board)
+    puts ""
     puts "  #{board[0]} | #{board[1]} | #{board[2]}"
     puts "----------"
     puts "  #{board[3]} | #{board[4]} | #{board[5]}"
     puts "----------"
     puts "  #{board[6]} | #{board[7]} | #{board[8]}"
+    puts ""
 end
 
 
 class Player
-    def initialize(name)
-        @name = name
+    def initialize(marker : String)
+        @marker = marker
     end
 
-    def name
-        @name
+    def marker
+        @marker
     end
 end
 
@@ -23,4 +25,14 @@ x = Player.new "X"
 o = Player.new "O"
 
 
-display(board)
+while true
+    display(board)
+    puts "Please enter a choice 1 - 9:"
+    puts ""
+    u_input = gets
+
+
+    if u_input == "q"
+        break
+    end
+end
