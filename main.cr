379dc532e19dc82cfa7c593bc84a9a0045f7d8c6
Michael Tillman <mnamllit@gmail.com>
2022-03-26 20:49:54 -0500

    Added basic state management to allow two players to play.

--- main.cr
+++ main.cr
@@ -1,11 +1,11 @@
-board = ["", "", "", "", "", "", "", "", ""]
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
 def display(board)
     puts ""
     puts "  #{board[0]} | #{board[1]} | #{board[2]}"
-    puts "----------"
+    puts " -----------"
     puts "  #{board[3]} | #{board[4]} | #{board[5]}"
-    puts "----------"
+    puts " -----------"
     puts "  #{board[6]} | #{board[7]} | #{board[8]}"
     puts ""
 end
@@ -25,22 +25,53 @@ x = Player.new "X"
 o = Player.new "O"
 
 def board_input(player, position, board)
-    b_input = position.to_i - 1
-    
-    board[b_input] = player.marker
-end
-
-while true
-    display(board)
-    puts "Please enter a choice 1 - 9:"
-    puts ""
-    u_input = gets
-
-
-    if u_input == "q"
-        break
+    if position == "1"
+        board[0] = player.marker
+    elsif position == "2"
+        board[1] = player.marker
+    elsif position == "3"
+        board[2] = player.marker
+    elsif position == "4"
+        board[3] = player.marker
+    elsif position == "5"
+        board[4] = player.marker
+    elsif position == "6"
+        board[5] = player.marker
+    elsif position == "7"
+        board[6] = player.marker
+    elsif position == "8"
+        board[7] = player.marker
+    elsif position == "9"
+        board[8] = player.marker
     end
 end
 
 
-display(board)
+player_state = 0
+
+while true
+    display(board)
+    puts ""
+    if player_state == 0
+        puts "Player 1, Please select a choice between 1 - 9"
+    else
+        puts "Player 2, Please select a choice between 1 - 9"
+    end
+    u_input = gets
+
+    if u_input == "q"
+        break
+    end
+
+    if player_state == 0
+        board_input(x, u_input, board)
+    else
+        board_input(o, u_input, board)
+    end
+    
+    if player_state == 0
+        player_state = 1
+    else
+        player_state = 0
+    end
+end
